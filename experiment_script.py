d95ec360747c0e800584690af7f8b809de61a123
jacobnrowen <53070635+jacobnrowen@users.noreply.github.com>
2020-12-15 09:58:21 -0500

    Update experiment_script.py

--- experiment_script.py
+++ experiment_script.py
@@ -5,7 +5,7 @@ import pandas as pd
 import glob
 import random
 
-test_types = [...]
+test_types = ['BareVowel','SameCons','DiffCons','Words']
 tests_per_typ = ...
 data_rows = []
 
@@ -19,7 +19,7 @@ def ask_about(data_logger, fname_a, fname_b, test_type, vowl_name, is_same, othe
 	print('----------')
 	print('Listen to these sounds.')
 	play_vowls(fname_a, fname_b)
-	print("Were the two vowel sounds the same or different? Type 'same' if they were the same, or 'different' if they were different.")
+	print("Were the vowel sounds the same or different? Type 'same' if they were the same, or 'different' if they were different.")
 	answer = ''
 	while answer != 'same' and answer != 'different':
 		answer = input()
@@ -31,11 +31,12 @@ def ask_about(data_logger, fname_a, fname_b, test_type, vowl_name, is_same, othe
 		data_logger.append( [test_type, vowl_name, 'N/A', is_correct] )
 	else:
 		is_correct = answer == 'different'
-		data_logger.append( [test_type, vowl_name, other_vowl, is_correct] )
+		data_logger.append( [test_type, vowl_name, other_vowl, is_correct, fname_a, fname_b] )
 
 for typ in test_types:
 	all_sounds = glob.glob(typ+'*')
-	all_vowels = { i[len(typ)+1:-6] for i in all_sounds }
+	all_vowels = { i[len(typ)+1:i.rfind('_')] for i in all_sounds }
+	print('This is the start of a new section of the experiment.')
 	for i in range(tests_per_typ):
 		if random.choice([True, False]):
 			vowl = random.choice(all_vowels)
@@ -45,8 +46,11 @@ for typ in test_types:
 			vowels = random.sample(all_vowels, 2)
 			fnames = [ random.choice(glob.glob(typ + '_' + v + '_*')) for v in vowels]
 			ask_about(data_rows, fnames[0], fnames[1], typ, vowels[0], False, vowels[1])
+	print("This is the end of a section of the experiment. Type OK when ready to proceed.")
+	while input() != 'OK':
+		print('Type OK when ready to proceed.')
 
-final_results = pd.DataFrame(data_rows, columns = ['Test Type', 'Vowel', 'Other Vowel', 'Correctness'])
+final_results = pd.DataFrame(data_rows, columns = ['Test Type', 'Vowel', 'Other Vowel', 'Correctness', 'File One', 'File Two'])
 print("Enter name of subject.")
 subj = input()
 final_results.to_csv(subj)
